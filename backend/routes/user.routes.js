88dabb8efe11ad518206126d22f64423cb71114b
Miguel Henriques <miguelaghenriques@gmail.com>
2024-03-12 22:41:28 +0000

    cloudinary added for image handling

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const fileUploader = require('../config/cloudinary.config');
 const User = require("../models/User.model")
 
 router.get("/users", (req, res, next) => {
@@ -15,6 +15,18 @@ router.get("/user/:id", (req, res, next) => {
     .then((user) => res.json(user))
     .catch((err) => next(err))
 })
+
+//Route to upload images using cloudinary
+router.post('/upload', fileUploader.single('imageUrl'), async (req, res, next) => {
+    
+
+    if(!req.file){
+     next(new Error('No file upload'));
+     return;
+    }
+ 
+    res.json({fileUrl : req.file.path})
+ })
 
 router.put("/user/:id", (req, res, next) => {
     const {id} = req.params;
